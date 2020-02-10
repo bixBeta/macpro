f85aab6712887214d99168e48eb9fe51fb09ef6a
faraz <fahmed@cornell.edu>
2020-02-10 18:57:13 -0500

    dos

--- cellRanger2.sh
+++ cellRanger2.sh
@@ -1,16 +1,57 @@
-cat HCJL5BGXB_Fogarty_10416629_cellranger_mkfastq_output_5June19//outs/input_samplesheet.csv | cut -d "," -f3 | cut -d "-" -f2 | sed 1,2d
-
-for i in */; do cd $i/outs; echo `pwd` >> ../../out.paths; cd  ../..; done
-
-
+#----------------------------------------------
+# get sample names and sample ids
 
 for i in */
 do
 	cd $i/outs
 	DIR=`pwd`
-	cat input_samplesheet.csv | cut -d "," -f3 | cut -d "-" -f2 | sed 1,2d 
+	cat input_samplesheet.csv | cut -d "," -f3 | cut -d "-" -f2 | cut -d "_" -f3 | sed 1,2d >> ../../.ids
+	cat input_samplesheet.csv | cut -d "," -f3 | sed 1,2d >> ../../.names
 	cd ../../
 
 done
 
+pwd
 
+
+#----------------------------------------------
+# samples with  multiple run folders
+
+sort .ids | uniq -d > .idoi  
+
+
+#----------------------------------------------
+# get fastqs path info 
+
+for i in  */
+do
+	cd $i/outs
+	cat input_samplesheet.csv | cut -d "," -f3 | sed 1,2d | awk '{print "'$i'" "outs/fastq_path/ :" $0}' >> ../../.paths.info
+	cd ../../
+
+done
+
+pwd
+
+wc -l .idoi
+
+readarray sampleIDs < .idoi
+
+for i in "${sampleIDs[@]}"
+do
+
+	ID=$i
+	echo "SAMPLE_ID = $ID "
+	GREP_PATH=$(grep `echo $i`$ .paths.info | cut -d ":" -f1  | xargs | sed -e 's/ /,/g')
+	echo "FASTQ_PATH = $GREP_PATH" 
+
+	GREP_NAME=$(grep `echo $i`$ .names | xargs | sed -e 's/ /,/g')
+	echo  "SAMPLE_NAME = $GREP_NAME"
+
+
+done
+
+
+
+
+ # rm .idoi .ids .names .paths.info 	
