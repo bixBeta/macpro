22228939c6a710ce1b114f77ad52e353d9f1f195
faraz <33409348+bixBeta@users.noreply.github.com>
2020-07-21 14:16:25 -0400

    Update saf_merge.R

--- saf_merge.R
+++ saf_merge.R
@@ -13,21 +13,17 @@ suppressPackageStartupMessages(library("dplyr"))
 saf.path <- args[1]
 file.path <- args[2]
 
-# saf.path <- "allSamplesMergedPeakset.Annotated.saf"
-# file.path <- "itkTh17_vs_wtTh17.complete.txt"
-# 
-# 
-# file.path <- "~/Downloads/itkTh17_vs_wtTh17.csv"
-# 
 
 ### read in the saf file
 safFile <- read.table(saf.path, header = T, sep = "\t")
 colnames(safFile)[1] <- "peakID" 
 
+
 if (grepl(".txt$", file.path)) {
   ### read in the sartools file 
   contrast <- read.table(file.path, header = T, sep = "\t")
 } else {
+  ### read in the shiny csv file
   contrast <- read.csv(file.path, header = T, row.names = 1)
   colnames(contrast)[1] <- "Id" 
   
@@ -35,12 +31,14 @@ if (grepl(".txt$", file.path)) {
 
 merged.results <- left_join(contrast, safFile, by = c("Id" = "peakID"))
 merged.results <- merged.results %>% select("Chr","Start", "End", "Id", everything())
-j = merged.results$Chr
 
 merged.results$Chr <- paste0('chr', merged.results$Chr)
 
 out.name <- strsplit(file.path, "\\.")[[1]][1]
 
+### Write the merged annotated output to file
 write.csv(merged.results, paste0(out.name,".ANNOTATED.csv"), quote = F, row.names = F)
+
 merged.results.bed <- merged.results  %>% select("Chr","Start", "End", "Id")
+### Write bed file to use with HOMER
 write.table(merged.results.bed, paste0(out.name,".homer.motif.input.bed"), quote = F, row.names = F, col.names = F, sep = "\t")
