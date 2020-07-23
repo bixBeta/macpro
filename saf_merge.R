02a03821a7a49e446a6008dace470f92f89683e5
bixBeta <fahmed02@icloud.com>
2020-07-23 11:40:23 -0400

    loopable on inputs

--- saf_merge.R
+++ saf_merge.R
@@ -14,31 +14,72 @@ saf.path <- args[1]
 file.path <- args[2]
 
 
+
+# SAF ----------------------------------------------------------------------------------------
+
+
 ### read in the saf file
 safFile <- read.table(saf.path, header = T, sep = "\t")
 colnames(safFile)[1] <- "peakID" 
 
 
-if (grepl(".txt$", file.path)) {
-  ### read in the sartools file 
-  contrast <- read.table(file.path, header = T, sep = "\t")
-} else {
-  ### read in the shiny csv file
-  contrast <- read.csv(file.path, header = T, row.names = 1)
-  colnames(contrast)[1] <- "Id" 
+# Merge Function ----------------------------------------------------------------------------
+
+mergeFunc <- function(s = safFile, c = contrast){
+  
+  merged.results <<- left_join(contrast, safFile, by = c("Id" = "peakID"))
+  merged.results <<- merged.results %>% select("Chr","Start", "End", "Id", everything())
+  
+  merged.results$Chr <<- paste0('chr', merged.results$Chr)
+  
+  # out.name <<- strsplit(file.path, "\\.")[[1]][1]
+  # 
+  # ### Write the merged annotated output to file
+  # write.csv(merged.results, paste0(out.name,".ANNOTATED.csv"), quote = F, row.names = F)
+  # 
+  # merged.results.bed <<- merged.results  %>% select("Chr","Start", "End", "Id")
+  # ### Write bed file to use with HOMER
+  # write.table(merged.results.bed, paste0(out.name,".homer.motif.input.bed"), quote = F, row.names = F, col.names = F, sep = "\t")
   
 }
 
-merged.results <- left_join(contrast, safFile, by = c("Id" = "peakID"))
-merged.results <- merged.results %>% select("Chr","Start", "End", "Id", everything())
 
-merged.results$Chr <- paste0('chr', merged.results$Chr)
 
-out.name <- strsplit(file.path, "\\.")[[1]][1]
 
-### Write the merged annotated output to file
-write.csv(merged.results, paste0(out.name,".ANNOTATED.csv"), quote = F, row.names = F)
+# Call Functions ---------------------------------------------------------------------------
 
-merged.results.bed <- merged.results  %>% select("Chr","Start", "End", "Id")
-### Write bed file to use with HOMER
-write.table(merged.results.bed, paste0(out.name,".homer.motif.input.bed"), quote = F, row.names = F, col.names = F, sep = "\t")
+file.path = list.files(path = ".", pattern = "_vs_")
+
+
+for (i in 1:length(file.path)) {
+  
+  if (grepl(".txt$", file.path[i])) {
+    ### read in the sartools file 
+    contrast <<- read.table(file.path[i], header = T, sep = "\t")
+  } else {
+    ### read in the shiny csv file
+    contrast <<- read.csv(file.path[i], header = T, row.names = 1)
+    colnames(contrast)[1] <- "Id" 
+  }
+  
+  mergeFunc(s = safFile, c = contrast)
+  
+  out.name <<- strsplit(file.path[i], "\\.")[[1]][1]
+  
+  ### Write the merged annotated output to file
+  write.csv(merged.results, paste0(out.name,".ANNOTATED.csv"), quote = F, row.names = F)
+  
+  merged.results.bed <<- merged.results  %>% select("Chr","Start", "End", "Id")
+  ### Write bed file to use with HOMER
+  write.table(merged.results.bed, paste0(out.name,".homer.motif.input.bed"), quote = F, row.names = F, col.names = F, sep = "\t")
+  
+}
+
+
+
+
+
+
+
+
+
